88fe2960010b456f9c3ae16b899d42fb2770eda6
GraydonShevchenko <Graydon.Shevchenko@gov.bc.ca>
2023-12-04 11:52:33 -0800

    Fix bug in rename attachments function call

--- trapper_data_collection/trapper_data_collection.py
+++ trapper_data_collection/trapper_data_collection.py
@@ -163,7 +163,7 @@ class Traps:
         self.rename_attachments(ago_layer=self.ago_traps, layer_name='traps', fld_unique_id='SET_UNIQUE_ID',
                                 fld_picture='PICUTRE', photo_prefix='trapsetup')
         
-        self.rename_attachments(ago_layer=self.ago, layer_name='trap checks', fld_unique_id='SET_UNIQUE_ID', fld_picture='PICTURE', photo_prefix='trapcheck')
+        self.rename_attachments(ago_layer=self.ago_traps, layer_name='trap checks', fld_unique_id='SET_UNIQUE_ID', fld_picture='PICTURE', photo_prefix='trapcheck')
 
         
 
